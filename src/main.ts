d37fe7caeded90377e2d7740133349229b25b9a8
Mikko Levonmaa <mikko.levonmaa@gmail.com>
2023-08-23 16:15:09 +0300

    Testing

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { debug, getInput, setFailed } from '@actions/core';
+import { parse } from 'yaml';
+const { promises: fs } = require('fs');
+
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const bump: string = getInput('bump');
+    debug(`Should bump ${bump}`);
+    const pubspecLocation = `${process.env.GITHUB_WORKSPACE}/pubspec.yaml}`;
+    debug(`READING ${pubspecLocation}`);
+    const pubspec = await fs.readFile(`${pubspecLocation}`, 'utf8')
+    debug(`Pubspec ${pubspec}`);
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const pubspecYaml = parse(pubspec);
+    debug(`Pubspec parsed ${pubspecYaml}`);
+    debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) setFailed(error.message)
   }
 }
 
